709e606409cf2208d5089951109bc3bc765d5adc
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2024-02-06 11:41:47 +0100

    Update3 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,37 +11,38 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
+	dlistint_t *current = *head;
 
-    if (*head == NULL)
-        return -1;
+	if (*head == NULL)
+		return -1;
 
-    if (index == 0)
-    {
-        if (*head != NULL)
-        {
-            *head = (*head)->next;
-            if (*head != NULL)
-                (*head)->prev = NULL;
-            free(current);
-            return 1;
-        }
-        return -1;
-    }
+	if (index == 0)
+	{
+		if (*head != NULL)
+		{
+			*head = (*head)->next;
+			if (*head != NULL)
+				(*head)->prev = NULL;
+			free(current);
+			return 1;
+		}
+		return -1;
+	}
 
-    while (index > 0 && current != NULL)
-    {
-        current = current->next;
-        index--;
-    }
+	while (index > 0 && current != NULL)
+	{
+		current = current->next;
+		index--;
+	}
 
-    if (current == NULL)
-        return -1;
+	if (current == NULL)
+		return -1;
 
-    current->prev->next = current->next;
-    if (current->next != NULL)
-        current->next->prev = current->prev;
-    free(current);
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
 
-    return 1;
+	return 1;
 }
+
