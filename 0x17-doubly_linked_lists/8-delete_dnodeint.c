435f41e6c7e938f4955ad4dc13b0efd00d72985f
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2024-02-06 11:44:48 +0100

    Update4 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current = *head;
 
 	if (*head == NULL)
-		return -1;
+		return (-1);
 
 	if (index == 0)
 	{
@@ -24,9 +24,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if (*head != NULL)
 				(*head)->prev = NULL;
 			free(current);
-			return 1;
+			return (1);
 		}
-		return -1;
+		return (-1);
 	}
 
 	while (index > 0 && current != NULL)
@@ -36,13 +36,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 
 	if (current == NULL)
-		return -1;
+		return (-1);
 
 	current->prev->next = current->next;
 	if (current->next != NULL)
 		current->next->prev = current->prev;
 	free(current);
 
-	return 1;
+	return (1);
 }
+
 
