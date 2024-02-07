237f0defefe7e8fad425819e83765c95186a07bc
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2024-02-07 09:58:14 +0100

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/bash
-gcc -Wall -c -fPIC *.c 
-gcc  -shared *.o  -o liball.so 
+#!/bin/bash
+
+# Compile all .c files in the current directory into object files
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create the dynamic library liball.so from the object files
+gcc -shared -o liball.so *.o
+
+# Clean up by removing the object files
+rm *.o
