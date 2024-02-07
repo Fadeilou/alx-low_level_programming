03f5739527fbb023c1ad9b24e949b1cc8748fa60
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2024-02-07 12:44:32 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo '#include <stdlib.h>' > /tmp/rand.c
-echo '#include <time.h>' >> /tmp/rand.c
-echo '#include <unistd.h>' >> /tmp/rand.c
-echo '__attribute__((constructor)) void init(void) { srand(time(NULL) + getpid()); }' >> /tmp/rand.c
-echo 'int rand(void) { return 9; }' >> /tmp/rand.c
-gcc -Wall -fPIC -shared -o /tmp/rand.so /tmp/rand.c
-export LD_PRELOAD=/tmp/rand.so
+wget -P /tmp/ https://raw.https://github.com/Fadeilou/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD =/tmp/nrandom.so
