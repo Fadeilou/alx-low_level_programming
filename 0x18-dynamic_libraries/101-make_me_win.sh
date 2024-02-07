36e4669077efa39113658c786975a5bc8f46c016
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2024-02-07 12:43:00 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-echo -e '#include <stdlib.h>\n#include <time.h>\n#include <unistd.h>\n__attribute__((constructor)) void init(void) { srand(time(NULL) + getpid()); }\nint rand(void) { return 9; }' > /tmp/rand.c
+echo '#include <stdlib.h>' > /tmp/rand.c
+echo '#include <time.h>' >> /tmp/rand.c
+echo '#include <unistd.h>' >> /tmp/rand.c
+echo '__attribute__((constructor)) void init(void) { srand(time(NULL) + getpid()); }' >> /tmp/rand.c
+echo 'int rand(void) { return 9; }' >> /tmp/rand.c
 gcc -Wall -fPIC -shared -o /tmp/rand.so /tmp/rand.c
 export LD_PRELOAD=/tmp/rand.so
